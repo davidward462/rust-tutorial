c0c92192d4ea33a5a1508e9966df8f3e6a3a1fd9
David Ward <davidmward@mail.com>
2024-03-05 13:30:22 -0800

    add to strings topic

--- strings/src/main.rs
+++ strings/src/main.rs
@@ -1,8 +1,37 @@
 fn main()
 {
+    // ways to create a string
+    
+    // string literal
     let phrase = "hello there";
 
     let s = phrase.to_string();
-    println!("{s}");
+    
+    let t = String::from("zeta");
+
+    // appending to string
+    let mut u = String::from("One");
+    
+    // takes string slice as parameter
+    u.push_str("Two");
+
+    // takes a single character
+    u.push('X');
+
+    println!("{s}\n{t}\n{u}");
+
+    // concatenate existing strings
+    let first = String::from("Bob");
+    let last = String::from("Smith");
+    let full = first + &last;
+    println!("{full}");
+    // note that 'first' is no longer valid
+    
+    // format macro
+    let a = String::from("Alpha");
+    let b = String::from("Beta");
+    let c = String::from("Gamma");
+    let d = format!("{a} {b} {c}");
+    println!("{d}");
 
 }
