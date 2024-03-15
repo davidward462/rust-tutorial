bbe21d0cf09ca4c1c90a49c4169564f5afbdff6c
David Ward <davidmward@mail.com>
2024-03-15 13:31:01 -0700

    add code for int sequence

---
+++ int-sequence/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+
+    let mut int_seq: Vec<i32> = Vec::new();
+
+    let length = 10;
+
+    for i in 0..10
+    {
+        int_seq.push(i);
+    }
+
+    println!("{:?}", int_seq);
+
+}
