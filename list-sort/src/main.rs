da58559d61b33334a1f41d51a027bef83b53cddf
David Ward <davidmward@mail.com>
2024-03-20 12:19:11 -0700

    create vector an get max

--- list-sort/src/main.rs
+++ list-sort/src/main.rs
@@ -5,9 +5,40 @@ fn rand_int(min: i32, max: i32) -> i32
     rand::thread_rng().gen_range(min..max)
 }
 
+fn max(vec: &Vec<i32>) -> i32
+{
+    let mut max: i32 = vec[0];
+    for &value in vec
+    {
+        if value > max
+        {
+            max = value
+        }
+    }
+    max
+}
+
+
 fn main() {
 
-    let value: i32 = rand_int(0, 10);
+    println!("-- Sort --");
 
-    println!("{value}");
+    let mut seq: Vec<i32> = Vec::new();
+    let length = 24;
+    let lower_bound = 0;
+    let upper_bound = 100;
+
+    // Populate vector with random values
+    for _i in 0..length
+    {
+        seq.push(rand_int(lower_bound, upper_bound));
+    }
+
+    // get max 
+    let max: i32 = max(&seq);
+    
+    println!("before: {:?}", seq);
+    println!("max: {max}");
+    
+
 }
