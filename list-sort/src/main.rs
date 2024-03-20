90754755cf3dbd5e0dd8ba3e260dc3d65992c5fe
David Ward <davidmward@mail.com>
2024-03-20 11:47:27 -0700

    add random int function

--- list-sort/src/main.rs
+++ list-sort/src/main.rs
@@ -1,3 +1,13 @@
+use rand::Rng;
+
+fn rand_int(min: i32, max: i32) -> i32
+{
+    rand::thread_rng().gen_range(min..max)
+}
+
 fn main() {
-    println!("Hello, world!");
+
+    let value: i32 = rand_int(0, 10);
+
+    println!("{value}");
 }
