d92cb1bdb530dee4734f799641b44a07c6333c03
David Ward <davidmward@mail.com>
2024-03-20 11:07:33 -0700

    add rng crate to int-sequence

--- int-sequence/src/main.rs
+++ int-sequence/src/main.rs
@@ -1,3 +1,11 @@
+use rand::Rng;
+
+// generate random i32 between min and max arguments
+fn random_int(min:i32, max:i32) -> i32
+{
+    rand::thread_rng().gen_range(min..max)
+}
+
 fn average(vec: &Vec<i32>) -> f32
 {
     let mut sum:f32 = 0.0;
@@ -16,9 +24,10 @@ fn main() {
     let mut int_seq: Vec<i32> = Vec::new();
 
     let length = 10;
-    for i in 1..=length
+    for _i in 1..=length
     {
-        int_seq.push(i);
+        let value:i32 = random_int(0, 10);
+        int_seq.push(value);
     }
 
     let avg:f32 = average(&int_seq);
