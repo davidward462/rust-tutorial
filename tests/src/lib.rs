7f98e194cbac00def436b1523cc9bd010a950619
David Ward <davidmward@mail.com>
2024-04-28 22:43:07 -0700

    add more tests

--- tests/src/lib.rs
+++ tests/src/lib.rs
@@ -11,6 +11,30 @@ impl Rectangle {
     }
 }
 
+pub fn increment(value: i32) -> i32
+{
+    value + 1
+}
+
+pub fn greeting(name: &str) -> String
+{
+    format!("Hello {}", name)
+}
+
+pub struct Month {
+    day: i32,
+}
+
+impl Month {
+    pub fn set_day(day: i32) -> Month
+    {
+        if day < 1 || day > 31 {
+            panic!("Day must be between 1 and 31. Got {}.", day);
+        }
+        Month{day}
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -28,6 +52,43 @@ mod tests {
             height: 5,
         };
         assert!(larger.can_hold(&smaller));
+        assert!(!smaller.can_hold(&larger));
+    }
+
+    #[test]
+    fn test_increment()
+    {
+        assert_eq!(4, increment(3));
+        assert_eq!(0, increment(-1));
+    }
+
+    #[test]
+    fn test_greeting()
+    {
+        let result = greeting("John");
+        assert!(result.contains("John"));
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_set_day()
+    {
+        Month::set_day(35);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
